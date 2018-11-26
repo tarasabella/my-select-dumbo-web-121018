e8aec77400ae0d8e80f90bfc845bf740ec994e51
Tara Sabella <tarasabella@gmail.com>
2018-11-26 20:20:19 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,5 @@
+
+>>>>>>> b7cc80be169df6b76fefe0dd2d7147bfcabbec48
 def my_select(nums)
   i = 0
   new_collection = []
@@ -8,3 +10,17 @@ def my_select(nums)
   nums.select do |num| num.even?
 end 
   end 
+<<<<<<< HEAD
+=======
+=======
+def my_select(collection)
+i = 0 
+new_collection = [] 
+new_collection << yield(collection.select[i])
+i += 1 
+end 
+new_collection
+end
+end 
+>>>>>>> dafe0c43912abebf397c72517f9aa098c9e982ee
+>>>>>>> b7cc80be169df6b76fefe0dd2d7147bfcabbec48
