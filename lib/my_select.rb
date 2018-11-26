155fe8d5b622db8c3828b833b56b665cec8d73ea
Tara Sabella <tarasabella@gmail.com>
2018-11-26 20:21:16 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,15 +1,4 @@
 
-def my_select(nums)
-  i = 0
-  new_collection = []
-  while i < nums.length
-    new_collection << yield(nums[i])
-    i += 1
-  end
-  nums.select do |num| num.even?
-end 
-  end 
-
 def my_select(collection)
 i = 0 
 new_collection = [] 
