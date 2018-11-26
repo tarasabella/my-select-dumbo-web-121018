c213047676d57211e8bf1f586b773faf541e420c
Tara Sabella <tarasabella@gmail.com>
2018-11-26 20:24:28 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,11 @@
-def my_select(nums)
+def my_select(array)
   i = 0
-  new_collection = []
-  while i < nums.length
-    new_collection << yield(nums[i])
-    i += 1
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
   end
-  nums.select do |num| num.even?
-end 
-  end 
+  select
+end
