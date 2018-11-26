e2d27d7f798ea073231c1ab5d0c51df878a1c823
Tara Sabella <tarasabella@gmail.com>
2018-11-26 20:22:46 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,20 @@
 def my_select(collection)
 i = 0 
 new_collection = [] 
+while i < collection.length
 new_collection << yield(collection.select[i])
 i += 1 
 end 
 new_collection
+end
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
+  end
+  select
 end
